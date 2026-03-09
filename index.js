5fe16a204c39ff860552294eccd3cc0c836340e6
Gowtham0748 <gowthamgsl0748@gmail.com>
2026-03-09 19:31:12 +0530

    Added basic ui to weather app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import https from "https";
 
 dotenv.config();
 const app = express();
@@ -23,28 +24,42 @@ app.get("/",(req,res)=>{
 app.post("/submit", async(req,res)=>{
     const city = req.body.city;
     const apikey = process.env.OPENWEATHER_API_KEY;
-    
+    //console.log("API KEY:", apikey);
     try{
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
+        const httpsAgent = new https.Agent({ family: 4 });
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`,
+            { httpsAgent }
+        );
 
         const result = response.data;
 
         res.render("index.ejs",{
             weather:{
                 city : result.name,
-                temperature: (result.main.temp - 273.15).toFixed(2),
+                temperature: result.main.temp,
                 description: result.weather[0].description,
                 humidity: result.main.humidity,
-                wind: result.wind.speed
+                wind: result.wind.speed,
+                icon:result.weather[0].icon,
             },
             error:null
         });
-    }catch(error){
-        console.log("failed to make request:",error.message);
-        res.render("index.ejs",{
-            weather:null,
-            error: "city not found",
-        });
+        }catch(error){
+            if (error.response) {
+            console.log("API RESPONSE ERROR:", error.response.data);
+            } 
+            else if (error.request) {
+                console.log("NO RESPONSE FROM API:", error.request);
+            } 
+            else {
+                console.log("ERROR:", error.message);
+            }
+
+            res.render("index.ejs", {
+                weather: null,
+                error: "City not found or API request failed"
+            });
+
     }
 
 });
